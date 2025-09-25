fd6f0d0a906a2fe2d8a9b5a406fec9aed499c34a
Lázaro Campos <lazaromanuel65@gmail.com>
2025-09-25 12:40:42 -0400

    Update UrlController.java

--- src/main/java/com/lazxnet/urlcraft/controller/UrlController.java
+++ src/main/java/com/lazxnet/urlcraft/controller/UrlController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Slf4j
-@CrossOrigin(value = "http://localhost:5173")
+@CrossOrigin(value = "https://urlcraft.onrender.com")
 @RestController
 public class UrlController {
 
